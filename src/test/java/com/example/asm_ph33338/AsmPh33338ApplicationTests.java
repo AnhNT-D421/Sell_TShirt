2dbfd823b21beab544577ac14e01f7905f861b30
Nguyễn Tuấn Anh <150307192+AnhNT-D421@users.noreply.github.com>
2024-07-09 04:27:42 +0700

    Update AsmPh33338ApplicationTests.java

--- src/test/java/com/example/asm_ph33338/AsmPh33338ApplicationTests.java
+++ src/test/java/com/example/asm_ph33338/AsmPh33338ApplicationTests.java
@@ -1,12 +1,5 @@
 package com.example.asm_ph33338;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class AsmPh33338ApplicationTests {
-
-    @Test
     void contextLoads() {
     }
 
